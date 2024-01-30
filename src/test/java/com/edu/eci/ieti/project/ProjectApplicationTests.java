152ade075756a86c527e6182ac671213330a5104
JSebastianRod <juan.rodriguez-pen@mail.escuelaing.edu.co>
2024-01-30 18:10:45 -0500

    first commit

---
+++ src/test/java/com/edu/eci/ieti/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eci.ieti.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
